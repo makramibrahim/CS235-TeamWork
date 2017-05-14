dcc92588b725965300d58c13807da221e654442d
Santos-Jefferson <jefferson2004@gmail.com>
2017-05-13 21:57:33 -0300

    Update nowServing.cpp

--- CS235-TeamWork/week04/nowServing.cpp
+++ CS235-TeamWork/week04/nowServing.cpp
@@ -1,11 +1,10 @@
 /***********************************************************************
  * Implementation:
- *    NOW SERVING
+ *    Now Serving with deque created before
  * Summary:
- *    This will contain the implementation for nowServing() as well as any
- *    other function or class implementations you may need
+ *    This will have nowServing() as well as any
  * Author
- *    <your names here>
+ *    Jefferson Santos
  **********************************************************************/
 
 #include <iostream>     // for ISTREAM, OSTREAM, CIN, and COUT
@@ -13,6 +12,7 @@
 #include <cassert>      // for ASSERT
 #include "nowServing.h" // for nowServing() prototype
 #include "deque.h"      // for DEQUE
+#include "servingStruct.h"   //for helping nowServing.h
 using namespace std;
 
 /************************************************
@@ -30,9 +30,102 @@ void nowServing()
    cout << "\tfinished                     : end simulation\n";
 
    // your code here
+   int timer = 0;
+   cout << "<" << timer << "> ";
+   
+   //
+   sentData servingRequest;
+   cin >> servingRequest.userClass;
+   cin >> servingRequest.name;
+   cin >> servingRequest.time;
+   servingRequest.empty = false;
+   
+   //
+   cout << "\tCurrently serving " << servingRequest.name << " for class " <<
+   servingRequest.userClass;
+   cout << ". Time left: " << servingRequest.time << endl;
+   timer++;
+   servingRequest.time--;
+   
+   //
+   Deque<sentData> nowServing;
+   for(;;)
+   {
+      string first;
+      sentData sentData;
+      
+      //
+      cout << "<" << timer << "> ";
+      
+      //
+      cin >> first;
+      
+      if (first == "!!")
+      {
+         sentData.emergency = true;
+         cin >> sentData.userClass;
+         cin >> sentData.name;
+         cin >> sentData.time;
+         sentData.empty = false;
+         nowServing.push_front(sentData);
+      }
+      else if (first == "none")
+      {
+         sentData.empty = true;
+      }
+      else if (first == "finished")
+      {
+         cout << "End of simulation\n";
+         return;
+      }
+      else 
+      {
+         sentData.emergency = false;
+         sentData.userClass = first;
+         cin >> sentData.name;
+         cin >> sentData.time;
+         sentData.empty = false;
+         nowServing.push_back(sentData);
+      }
+      
+      if (!nowServing.empty())
+      {
+         if (servingRequest.time <= 0)
+         {
+            servingRequest = nowServing.front();
+            nowServing.pop_front();
+         }
+         if (servingRequest.emergency)
+         {
+            cout << "\tEmergency for " << servingRequest.name << " for class " <<
+            servingRequest.userClass;
+            cout << ". Time left: " << servingRequest.time << endl;
+         }
+         else
+         {
+            cout << "\tCurrently serving " << servingRequest.name << " for class " << servingRequest.userClass;
+            cout << ". Time left: " << servingRequest.time << endl;
+         }
+      }
+      else if (servingRequest.time > 0)
+      {
+         if (servingRequest.emergency)
+         {
+            cout << "\tEmergency for " << servingRequest.name << " for class " << servingRequest.userClass;
+            cout << ". TIme left: " << servingRequest.time << endl;
+         }
+         else
+         {
+            cout << "\tCurrently serving " << servingRequest.name << " for class " << servingRequest.userClass;
+            cout << ". Time left: " << servingRequest.time << endl;
+         }
+      }
+      
+      //
+      timer++;
+      servingRequest.time--;
+   }
 
    // end
    cout << "End of simulation\n";
 }
-
-
